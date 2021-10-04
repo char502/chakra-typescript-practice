75642f3da6ed9b7be8a0e8b4587bb74570541c8b
char502 <char502@hotmail.com>
2021-10-04 23:13:45 +0100

    addded details and cart components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,26 @@
-const IndexPage = () => <h1>Hello Next.js 123</h1>
+import { Container, Flex } from '@chakra-ui/react';
+import React from 'react';
+import Cart from '../src/sections/cart';
+import Details from '../src/sections/details';
 
-export default IndexPage
+const IndexPage = () => (
+  // the container component by default has a padding of 16px which don't want so turn it off
+  // Container component to keep content contained and centered
+  // Can use Flec and Vertical Stack to stack the inner elements
+  // when want to add extra styling, can use the style props which have access to the design tokens
+  // built in Chakra Ui
+  <Container maxW='container.xl' p={0}>
+    <Flex
+    // If only want to change between fewer values, use object syntax (because it's simpler)
+      h={{ base: 'auto', md: '100vh' }}
+      // use array syntax if want to provide different values for every breakpoint
+      py={[0, 10, 20]}
+      direction={{ base: 'column-reverse', md: 'row' }}
+    >
+      <Details />
+      <Cart />
+    </Flex>
+  </Container>
+);
+
+export default IndexPage;
